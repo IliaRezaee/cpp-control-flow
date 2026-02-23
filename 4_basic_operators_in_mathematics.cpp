2a3a087df12105f60951a8c935d0f543f81853e3
Ilia Rezaee <iliarezaeebta@gmail.com>
2026-02-24 00:25:14 +0330

    Add files via upload

---
+++ 4_basic_operators_in_mathematics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   cout << 100+10 << "\n";
+   cout << 40-20 << "\n";
+   cout << 6*6 << "\n";
+   cout << 98/2 << "\n";
+
+   return 0;
+}
